e1bacf6049a5fea51433da77ad29a14d3695cf7d
cyborggeneraal <alexvantilburg@outlook.com>
2024-03-07 12:46:12 +0100

    worked in lecture 5

--- l5_unimath_exercises.v
+++ l5_unimath_exercises.v
@@ -4,6 +4,57 @@ Require Export UniMath.Foundations.All.
 
 (* Show Lemma 11.1.2 of Rijke. Give Def 11.1.1 yourself. *)
 
+Definition tot {A : UU} {B C : A -> UU} 
+  (f : ∏ x : A, B x -> C x) :
+  (∑ x : A, B x) -> (∑ x : A, C x).
+Proof.
+  intros [x y].
+  exact (x,, f x y).
+Defined.
+
+Theorem equiv_fib {A : UU} {B C : A -> UU}
+  (f : ∏ x : A, B x -> C x) (t : ∑ x : A, C x) :
+  hfiber (tot f) t ≃ hfiber (f (pr1 t)) (pr2 t).
+Proof.
+  use tpair.
+  - simpl.
+    intros [[x' y'] p].
+    use tpair.
+  -- unfold tot in p.
+     induction p.
+     simpl.
+     exact y'.
+  -- unfold tot in p.
+     simpl.
+     induction p.
+     simpl.
+     apply idpath.
+  - simpl.
+    use isweq_iso.
+  -- intros [x' h].
+     use tpair.
+  --- induction t as [t1 t2].
+      simpl in x'.
+      exact (t1,, x'). 
+  --- induction t as [t1 t2].
+      simpl in *.
+      unfold tot.
+      induction h.
+      apply idpath.
+  -- intros [x y].
+     unfold tot in y.
+     induction y.
+     simpl.
+     induction x as [x1 x2].
+     apply idpath.
+  -- intros [x y].
+     induction t as [t1 t2].
+     simpl in *.
+     induction y.
+     simpl.
+     apply idpath.
+   Qed.
+
 (* Hint: Follow the proof in Rijke, using `isweq_iso` at the end. For the statements, you will want to use `hfiber`, `∘` (`funcomp`), `~` (`homot`), `≃` (`weq`). *)
 
 (* Exercise 2 *)
