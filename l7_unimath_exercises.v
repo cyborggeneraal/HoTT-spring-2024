b53ab31ab73acde297c7ccb1a7c4a2d71b822c65
cyborggeneraal <alexvantilburg@outlook.com>
2024-03-21 13:02:32 +0100

    worked in lecture

--- l7_unimath_exercises.v
+++ l7_unimath_exercises.v
@@ -11,28 +11,85 @@ Variable funext : funextsecStatement.
 Definition prop_trunc (X : UU) : UU := ∏ P : hProp, ((X -> P) -> P).
 
 Theorem prop_trunc_unit {X : UU} : X → prop_trunc X.
-Admitted.
+Proof.
+  intros x P f.
+  apply f.
+  exact x.
+Qed.
 
 (* Exercise 2 *)
 
 Theorem prop_trunc_unit_eq {X : UU} {x y : X} : prop_trunc_unit x = prop_trunc_unit y.
 Proof.
-Admitted.
+  apply funext.
+  intro P.
+  apply funext.
+  intro f.
+  apply P.
+Qed.
 
 (* Exercise 3 *)
 
 (* Use ~invproofirrelevance~ or what you have done before.*)
 
+Print invproofirrelevance.
+Print hProp.
+
 Theorem prop_trunc_prop {X : UU} : isaprop (prop_trunc X).
 Proof.
-Admitted.
+  use invproofirrelevance.
+  unfold isProofIrrelevant.
+  intros x y.
+  apply funext.
+  intro P.
+  apply funext.
+  intro f.
+  apply P.
+Qed.
+
 
 (* Exercise 4 *)
 
 (* Hint: use ~isapropimpl~ and ~isweqimplimpl~.*)
 
+Print isweqimplimpl.
+
 Theorem univ_prop_prop_trunc (T : UU) (P : hProp) : (T → P) ≃ (prop_trunc T → P).
-Admitted.
+Proof.
+  use tpair.
+  - intros f x.
+    apply x.
+    exact f.
+  - simpl.
+    apply isweqimplimpl.
+  -- intros f t.
+     apply f.
+     intros Q g.
+     apply g.
+     exact t.
+  -- apply isapropimpl.
+     unfold isaprop.
+     simpl.
+     intros x y.
+     induction P as [P h].
+     simpl in *.
+     apply h.
+  -- induction P as [P h].
+     simpl.
+     unfold isaprop.
+     simpl.
+     intros f g.
+     use tpair.
+  --- apply funext.
+      intro x.
+      apply h.
+  --- simpl.
+      intros [].
+      unfold isaprop in *.
+      simpl in *.
+
+    
+
 
 (* Exercise 5 *)
 
