f89c599c1f239dc70a03615872b2402b43063546
cyborggeneraal <alexvantilburg@outlook.com>
2024-03-25 12:49:38 +0100

    worked during afp

--- hw_2.v
+++ hw_2.v
@@ -19,8 +19,25 @@ Proof.
   Admitted.
 
 (* 1b. Exercise 9.2a *)
+Definition const {A : UU} (a : A) : A → A.
+Proof.
+  Admitted.
+
+Theorem Ex_9_2_a (b : bool) : isweq (const b).
+Proof.
+  Admitted.
+
+Search "retract".
 
 (* 1c. Exercise 9.4a *)
+Definition has_retract {A B : UU} (f : A → B) : UU := ∑ g : B → A, g ∘ f ~ idfun A.
+
+Theorem Ex_9_4_a {A B X : UU} (f : A → X) (g : B → X) (h : A → B)
+  (H : f ~ g ∘ h) : ∑ s : B → A, h ∘ s ~ idfun B → 
+  f ∘ s ~ g × 
+  (has_retract f <-> has_retract g).
+Proof.
+  Admitted.
 
 (* 1d. Exercise 9.9a *)
 
@@ -43,17 +60,54 @@ Proof.
 
 (* Show that the definitions of proposition are equivalent. *)
 
+Search "isweq".
+
+Definition f2 {P : UU} : (isaprop P) → (∏ x y : P, x = y).
+Proof.
+  intros h x y.
+  induction (h x y) as [c _].
+  exact c.
+Defined.
+
+Definition f2' {P : UU} : (∏ x y : P, x = y) → (isaprop P).
+Proof.
+  intros h x y.
+  simpl.
+  exists (h x y).
+  intro t.
+Admitted.
+
+
 Theorem prop_thm {P : UU} : (isaprop P) ≃ (∏ x y : P, x = y).
 Proof.
   Admitted.
 
 (* Exercise 3 *)
 
+Search "invmap".
+
 (* Proposition 12.1.4 from Rijke: An equivalence between propositions is (logically equivalent to) a logical equivalence. *)
 
 Theorem Prop_12_1_4 (P Q : hProp) : (P ≃ Q) <-> (P <-> Q).
 Proof.
-  Admitted.
+  split.
+  - intros h.
+    split.
+  -- induction h as [f _].
+     exact f.
+  -- exact (invmap h).
+  - intros [f g].
+    exists f.
+    eapply (isweq_iso f g).
+  -- intro x.
+     induction P as [P hp].
+     simpl in *.
+     apply hp.
+  -- intro y.
+     induction Q as [Q hq].
+     simpl in *.
+     apply hq.
+Qed.
 
 (* Exercise 4 *)
 
@@ -61,20 +115,77 @@ Proof.
 
 Theorem prop_commutes_Π {A : UU} {B : A → UU} (p : ∏ x : A, isaprop (B x)) : isaprop (∏ x : A, (B x)).
 Proof.
-  Admitted.
+  apply f2'.
+  intros f g.
+  apply funextsec.
+  intro x.
+  apply f2.
+  apply p.
+Qed.
 
 (* Exercise 5 *)
 
 (* Show that isweq f (is-contr f in Rijke) is a proposition. *)
 
+Lemma ex5 {A B : UU} {f : A → B} {y : B} {c : hfiber f y} (h1 h2 : ∏ t : hfiber f y, t = c) :
+  h1 = h2.
+Proof.
+  apply funextsec.
+  intro x.
+  induction (h1 x).
+  induction x as [x hx].
+Admitted.
+
 Theorem isweq_is_prop {A B : UU} (f : A → B) : isaprop (isweq f).
 Proof.
-  Admitted.
+  apply f2'.
+  intros p q.
+  unfold isweq in *.
+  apply funextsec.
+  intro x.
+  induction (p x) as [c hp].
+  induction (q x) as [c' hq].
+  induction (hq c).
+  induction (ex5 hp hq).
+  apply idpath.
+Qed.
 
 (* Exercise 6 *)
 
 (* An equivalence between propositions is (equivalent to) a logical equivalence. *)
 
+Definition f6 {P Q : hProp} : (P ≃ Q) → (P <-> Q).
+Proof.
+  intro h.
+  split.
+  - induction h as [f _].
+    exact f.
+  - exact (invmap h).
+Defined.
+
+Definition f6' {P Q : hProp} : (P <-> Q) → (P ≃ Q).
+Proof.
+  intros [f g].
+  exists f.
+  apply (isweq_iso f g).
+  - intro x.
+    induction P as [P hp].
+    simpl in *.
+    apply hp.
+  - intro y.
+    induction Q as [Q hq].
+    simpl in *.
+    apply hq.
+Defined.
+    
+
 Theorem equiv_of_prop {P Q : hProp} : (P ≃ Q) ≃ (P <-> Q).
 Proof.
-  Admitted.
+  exists f6.
+  apply (isweq_iso f6 f6').
+  - intros [f h].
+    unfold isweq in h.
+    unfold f6.
+    unfold f6'.
+    
+    
