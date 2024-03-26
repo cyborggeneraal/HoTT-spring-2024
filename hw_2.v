07eb37b49324eccf1ceee011f637002712214b4c
cyborggeneraal <alexvantilburg@outlook.com>
2024-03-26 15:30:44 +0100

    worked during alg top 2

--- hw_2.v
+++ hw_2.v
@@ -52,7 +52,9 @@ Proof.
 
 Lemma contr_to_path {C : UU} (h : iscontr C) (x y : C) : x = y.
 Proof.
-  Admitted.
+  induction h as [c h].
+  exact (h x @ ! h y).
+Qed.
 
 (* From here on, all `Admitted.`s should be filled in with proofs. As always, don't change the statements of any Theorems below, but you can always prove extra Lemmas to help. *)
 
@@ -72,15 +74,38 @@ Defined.
 Definition f2' {P : UU} : (∏ x y : P, x = y) → (isaprop P).
 Proof.
   intros h x y.
-  induction (h x y).
-Admitted.
+  simpl.
+  apply (@hlevel_cumulative 0).
+  simpl.
+  exists y.
+  intro t.
+  exact (h t y).
+Defined.
 
 
 Theorem prop_thm {P : UU} : (isaprop P) ≃ (∏ x y : P, x = y).
 Proof.
   exists f2.
-  intro y.
-Admitted.
+  apply (isweq_iso f2 f2').
+  - intro h.
+    unfold f2.
+    unfold f2'.
+    apply funextsec.
+    intro x.
+    apply funextsec.
+    intro y.
+    unfold isaprop in h.
+    simpl in h.
+    apply (isapropisofhlevel 0 (x = y)).
+  - intros f.
+    assert (H := hlevel_cumulative (f2' f)).
+    simpl in H.
+    apply funextsec.
+    intro x.
+    apply funextsec.
+    intro y.
+    apply (H x y).
+  Qed.
 
 (* Exercise 3 *)
 
@@ -132,6 +157,10 @@ Lemma ex5 {A B : UU} {f : A → B} {y : B} {c : hfiber f y} (h1 h2 : ∏ t : hfi
 Proof.
   apply funextsec.
   intro x.
+  apply contr_to_path.
+  exists (h1 x).
+  intro t.
+  induction t.
 Admitted.
 
 
@@ -196,20 +225,27 @@ Theorem equiv_of_prop {P Q : hProp} : (P ≃ Q) ≃ (P <-> Q).
 Proof.
   exists f6.
   apply (isweq_iso f6 f6').
-  - intros [f h].
-    unfold isweq in h.
-    unfold f6.
-    unfold f6'.
-    induction P as [P hp].
-    induction Q as [Q hq].
-    admit.
+  - intros h.
+    apply contr_to_path.
+    exists h.
+    intro t.
+    Search "isaprop_total".
+    unfold weq in h.
+    assert (isaprop (P -> Q)).
+  -- apply prop_commutes_Π.
+     intro p.
+     induction Q as [Q hq].
+     simpl.
+     exact hq.
+  -- induction (isaprop_total2 ((P → Q),, X) (λ x, (isweq x,, isweq_is_prop x)) t h) as [c _].
+     exact c. 
   - intros [h1 h2].
     unfold f6'.
     unfold f6.
     unfold invmap.
     simpl.
     apply idpath.
-  Admitted.
+  Qed.
     
     
     
